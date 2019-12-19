6fbab32c053590653cc7afc333959aea25d6aa92
CedricDeMeester <cedricje.demeester@gmail.com>
2019-12-19 17:44:45 +0100

    chart

--- scripts/chart.js
+++ scripts/chart.js
@@ -1,53 +1,6 @@
-const SERVER_ENDPOINT = 'https://api.nasa.gov/insight_weather/?api_key=dExwaBnXRzmESBZqFiDEsbKUHeugVAGj1X0wyrx2&feedtype=json&ver=1.0';
-
-let customHeaders = new Headers();
-customHeaders.append('Accept', 'application/json');
-
-const fetchData =function(){
-  return fetch(SERVER_ENDPOINT, {headers : customHeaders})
-      .then(r => r.json())
-      .then(data => data);
-}
-
-const getAPICoolestWay = async function(){
-  try
-  {
-  const data = await fetchData();
-  showdata(data);
-  }
-  catch(error)
-  {
-      console.warn(error)
-  }
-}
-
-
-const showdata = function(data){
-  console.log(data);
-	domData.innerHTML = "";
-  let HTML = 'test';
-  //HTML += <div class = "js-data" value ="${d}"> ${data} ${data[d].symbol} => ${d}</div>;
-	for (d in data) {
-        var sol = "";
-        for(i in d)
-        {
-          sol+= d[i];
-        }
-        if(sol == "sol_keys")
-        {
-          break;
-        }
-        else
-        {
-          for(e in i)
-          console.log(i[e])
-        }
-        sol = "";
-  }
-	domData.innerHTML += HTML;
-};
-
 var chartVars = "KoolOnLoadCallFunction=chartReadyHandler";
+
+
 
 function chartReadyHandler(id) {
   document.getElementById(id).setLayout(layoutStr);
@@ -111,15 +64,7 @@ var chartData =
   {"Month":"Nov","Seoul":900,"NewYork":390,"Paris":440},
   {"Month":"Dec","Seoul":600,"NewYork":680,"Paris":0}];
 
-const init = function()
-{
-  domData = document.querySelector('.js-data');
-  getAPICoolestWay();
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  console.info('DOM geladen');
-  KoolChart.create("chart1", "chartHolder", chartVars, "100%", "100%");
-  init();
-  
-});
+  document.addEventListener('DOMContentLoaded', function() {
+    console.info('DOM geladen2');
+    KoolChart.create("chart1", "chartHolder", chartVars, "100%", "100%");
+  });
