fdbc06ce857feeb1babb3570a18cbee3f26e68bd
cedric <cedricje.demeester@gmail.com>
2020-08-20 23:11:58 +0200

    added css

--- scripts/chart.js
+++ scripts/chart.js
@@ -1,70 +1,159 @@
-var chartVars = "KoolOnLoadCallFunction=chartReadyHandler";
+// Endpoint
+var SERVER_ENDPOINT = 'https://poloniex.com/public?command=returnChartData&currencyPair=BTC_ETH&start=0&end=9999999999&period=86400';
 
+// Add a few headers - UITZONDERING
+let customHeaders = new Headers();
+customHeaders.append('Accept', 'application/json');
 
-
-function chartReadyHandler(id) {
-  document.getElementById(id).setLayout(layoutStr);
-  document.getElementById(id).setData(chartData);
+const fetchData =function(end){
+  return fetch(end, {headers : customHeaders})
+      .then(r => r.json())
+      .then(data => data);
 }
 
-var layoutStr =
-  '<KoolChart backgroundColor="#FFFFFF"  borderStyle="none">'
-   +'<Options>'
-    +'<Caption text="Mars Wind" />'
-    +'<SubCaption text="( mm )" textAlign="right" />'
-    +'<Legend defaultMouseOverAction="true" useVisibleCheck="true"/>'
-   +'</Options>'
-   +'<WindRoseChart showDataTips="true" dataTipDisplayMode="mouse" paddingBottom="30">'
-    +'<angularAxis>'
-     +'<CategoryAxis id="aAxis" categoryField="Month"/>' 
-    +'</angularAxis>'
-    +'<series>'
-     +'<WindRoseSeries field="Seoul" displayName="Seoul">'
-      +'<fill>'
-       +'<SolidColor color="#49e1d2" alpha="0.8"/>'
-      +'</fill>'
-      +'<showDataEffect>'
-       +'<SeriesInterpolate duration="1000"/>' 
-      +'</showDataEffect>'
-     +'</WindRoseSeries>'
-     +'<WindRoseSeries field="NewYork" displayName="New York">'
-      +'<fill>'
-       +'<SolidColor color="#54bbe8" alpha="0.8"/>'
-      +'</fill>'
-      +'<showDataEffect>'
-       +'<SeriesInterpolate duration="1000"/>' 
-      +'</showDataEffect>'
-      +'</WindRoseSeries>'
-     +'<WindRoseSeries field="Paris" displayName="Paris">'
-      +'<fill>'
-       +'<SolidColor color="#5560af" alpha="0.8"/>'
-      +'</fill>'
-      +'<showDataEffect>'
-       +'<SeriesInterpolate duration="1000"/>' 
-      +'</showDataEffect>'
-     +'</WindRoseSeries>'
-    +'</series>'
-    +'<radialAxisRenderers>'
-     +'<Axis2DRenderer axis="{aAxis}"/>'
-    +'</radialAxisRenderers>'
-   +'</WindRoseChart>'
-  +'</KoolChart>';
+const getAPICoolestWay = async function(end){
+  try
+  {
+  Microinteraction();
+  const data = await fetchData(end);
+  showdata(data);
+  }
+  catch(error)
+  {
+      console.warn(error)
+  }
+}
 
-var chartData =
-  [{"Month":"Jan","Seoul":600,"NewYork":600,"Paris":400},
-  {"Month":"Feb","Seoul":800,"NewYork":400,"Paris":0},
-  {"Month":"Mar","Seoul":400,"NewYork":200,"Paris":0},
-  {"Month":"Apr","Seoul":400,"NewYork":420,"Paris":0},
-  {"Month":"May","Seoul":600,"NewYork":500,"Paris":0},
-  {"Month":"Jun","Seoul":900,"NewYork":380,"Paris":340},
-  {"Month":"Jul","Seoul":600,"NewYork":710,"Paris":430},
-  {"Month":"Aug","Seoul":600,"NewYork":360,"Paris":0},
-  {"Month":"Sep","Seoul":800,"NewYork":380,"Paris":0},
-  {"Month":"Oct","Seoul":600,"NewYork":700,"Paris":480},
-  {"Month":"Nov","Seoul":900,"NewYork":390,"Paris":440},
-  {"Month":"Dec","Seoul":600,"NewYork":680,"Paris":0}];
+const showdata = function(data){
+  const TimeList = []
+  const PriceList = []
 
-  document.addEventListener('DOMContentLoaded', function() {
-    console.info('DOM geladen2');
-    KoolChart.create("chart1", "chartHolder", chartVars, "100%", "100%");
-  });
+  for (d in data) 
+    {
+    const Price = data[d].weightedAverage;
+    const unix_timestamp = data[d].date;
+    
+    const a = new Date(unix_timestamp * 1000);
+    const months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    const year = a.getFullYear();
+    const month = months[a.getMonth()];
+    const date = a.getDate();
+    const Time = date + ' ' + month + ' ' + year + ' ';
+    TimeList.push(Time);
+    PriceList.push(Price);
+    }
+    EndMicrointeraction();
+    loadGraph(TimeList,PriceList);
+};
+
+const loadGraph = function (time,price)
+{
+let myChart = document.getElementById("myChart").getContext('2d');
+
+var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: time,
+        datasets: [{
+            label: 'Market Price (BTC)',
+            fill: true,
+            backgroundColor: 'rgb(143, 178, 220, 0.2)',
+            borderColor: 'rgb(143, 178, 220)',
+            data: price
+        }]
+    },
+    options: 
+    {
+    layout:
+    {
+      padding:
+      {
+        left:24,
+        right:48,
+      }
+    },
+    legend:
+    {
+        display: false,
+    },
+    elements: 
+    {
+       point: 
+      {
+         radius: 0 
+      } 
+    },
+    easing: 'easeInQuad'
+    }
+});
+}
+
+const Microinteraction = function()
+{
+
+  let myChart = document.getElementById("mychart");
+  domData.innerHTML = "";
+	let HTML = '<div class="microinteraction"><div></div><div></div><div></div></div>';
+  domData.innerHTML += HTML;
+}
+
+const EndMicrointeraction = function()
+{
+  let myChart = document.getElementById("mychart");
+  domData.innerHTML = "";
+  let HTML = '<div class="o-graph"><canvas id="myChart"></canvas></div>';
+  domData.innerHTML += HTML;
+}
+
+const AllTime = function () {
+  ENDPOINT = SERVER_ENDPOINT + "&start=" + "0" + "&end=9999999999&period=86400";
+  domTime.innerHTML = '<span class="js-time">all time</span>';
+  getAPICoolestWay(ENDPOINT);
+};
+
+const TwoYear = function () {
+  start = Math.floor(Date.now() / 1000);
+  start = start - 63113852;
+  ENDPOINT = SERVER_ENDPOINT + "&start=" + start + "&end=9999999999&period=86400";
+  domTime.innerHTML = '<span class="js-time">2 years</span>';
+  getAPICoolestWay(ENDPOINT);
+};
+
+const Year = function () {
+  start = Math.floor(Date.now() / 1000);
+  start = start - 31556926;
+  ENDPOINT = SERVER_ENDPOINT + "&start=" + start + "&end=9999999999&period=86400";
+  domTime.innerHTML = '<span class="js-time">1 year</span>';
+  getAPICoolestWay(ENDPOINT);
+};
+
+const Month = function () {
+  start = Math.floor(Date.now() / 1000);
+  start = start - 2629744;
+  ENDPOINT = SERVER_ENDPOINT + "&start=" + start + "&end=9999999999&period=86400";
+  domTime.innerHTML = '<span class="js-time">1 month</span>';
+  getAPICoolestWay(ENDPOINT);
+};
+
+
+
+
+const init = function()
+{
+  domTime = document.querySelector('.js-time');
+  domData = document.querySelector('.js-microI');
+  getAPICoolestWay(SERVER_ENDPOINT);
+  document.querySelector('.js-button-AllTime').addEventListener('click', AllTime);
+  document.querySelector('.js-button-2year').addEventListener('click', TwoYear);
+  document.querySelector('.js-button-1year').addEventListener('click', Year);
+  document.querySelector('.js-button-month').addEventListener('click', Month);
+
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  console.info('DOM geladen');
+  init();
+  
+});
